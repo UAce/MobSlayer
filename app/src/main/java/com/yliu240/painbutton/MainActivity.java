f4cb07e8d7f0e4090cb9dbd5128a185f572b9085
Yu-Yueh Liu <yu-yueh@hotmail.com>
2019-01-27 03:54:12 -0500

    Refactored Code

--- app/src/main/java/com/yliu240/painbutton/MainActivity.java
+++ app/src/main/java/com/yliu240/painbutton/MainActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Shader;
 import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.BoringLayout;
@@ -30,19 +31,100 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
 
+    int dmgTop,dmgBottom, critTop, critBottom;
+    int[] screenCenter = new int[2];
+    ImageButton sound = null;
+    MediaPlayer damageFx, bgm;
+    ImageView screenInFrontOfMob;
+    GifImageView mob;
+    RelativeLayout RL;
+    RelativeLayout.LayoutParams lp;
+//    Animation fadeIn, fadeOut;
+//    AnimationSet animationSet;
+    Shader textShader, critShader;
+    Typeface comic_sans;
+    final int critialDmgSize = 55;
+    final int normalDmgSize = 50;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Start Bgm and Sound fx
-        final MediaPlayer damageFx = MediaPlayer.create(MainActivity.this,R.raw.slime_damage_sound);
-        final MediaPlayer bgm = MediaPlayer.create(MainActivity.this,R.raw.ellinia_bgm1);
+        damageFx = MediaPlayer.create(MainActivity.this,R.raw.slime_damage_sound);
+        bgm = MediaPlayer.create(MainActivity.this,R.raw.ellinia_bgm1);
         bgm.start();
         bgm.setLooping(true);
 
         // Sound button
-        final ImageButton sound = (ImageButton) findViewById(R.id.sound);
+        sound = (ImageButton) findViewById(R.id.sound);
+        screenInFrontOfMob = (ImageView) findViewById(R.id.transparent);
+        mob = (GifImageView) findViewById(R.id.slimeGif);
+
+        // RelativeLayout holding the damageText
+        RL = (RelativeLayout) findViewById(R.id.relayout);
+        lp = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, // Width of TextView
+                RelativeLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+
+
+        //Colors For damageText
+        dmgTop=ContextCompat.getColor(MainActivity.this, R.color.dmgTop);
+        dmgBottom=ContextCompat.getColor(MainActivity.this, R.color.dmgBottom);
+        critTop=ContextCompat.getColor(MainActivity.this, R.color.critTop);
+        critBottom=ContextCompat.getColor(MainActivity.this, R.color.critBottom);
+        comic_sans = Typeface.createFromAsset(getAssets(),"comic-sans-ms-bold.ttf");
+        screenCenter[0]=this.getResources().getDisplayMetrics().widthPixels;
+        screenCenter[0]-=this.getResources().getDisplayMetrics().widthPixels/2;
+        screenCenter[1]=this.getResources().getDisplayMetrics().heightPixels;
+        screenCenter[1]-=this.getResources().getDisplayMetrics().heightPixels/2;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        bgmSoundListen(); //Listens to sound Button if pressed
+
+        screenInFrontOfMob.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch (View v, MotionEvent event){
+                switch (event.getAction()) {
+
+                    // Pressed
+                    case MotionEvent.ACTION_DOWN: {
+                        if(damageFx.isPlaying()){
+                            damageFx.seekTo(0);
+                        }
+                        damageFx.start();
+
+                        // Generate random Damage and create damage Text
+                        int damageTaken=0;
+                        int x = (int) event.getRawX();
+                        int y = (int) event.getRawY();
+                        //TODO: Make this relative to mob position
+                        if (screenCenter[0]+100>x && screenCenter[0]-300<x && screenCenter[1]-50<y && screenCenter[1]+350>y){
+                            damageTaken = ThreadLocalRandom.current().nextInt(500000, 999999 + 1);
+                        }
+                        createDamageText(damageTaken, x, y);
+
+                        mob.setImageResource(R.drawable.kingslimehurt);
+                        break;
+                    }
+                    // Released
+                    case MotionEvent.ACTION_UP: {
+                        mob.setImageResource(R.drawable.kingslime_animation);
+                        break;
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+    public void bgmSoundListen(){
         Boolean clicked = new Boolean(false);
         sound.setTag(clicked); // Button wasn't clicked
         sound.setOnClickListener(new View.OnClickListener() {
@@ -51,125 +133,108 @@ public class MainActivity extends AppCompatActivity {
                 if( ((Boolean)sound.getTag())==false ){
                     sound.setImageResource(R.drawable.baseline_volume_off_24);
                     bgm.pause();
-//                    Toast.makeText(MainActivity.this, "Mute", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Mute", Toast.LENGTH_SHORT).show();
                     sound.setTag(new Boolean(true));
                 }else{
                     sound.setImageResource(R.drawable.baseline_volume_up_24);
                     bgm.start();
-//                    Toast.makeText(MainActivity.this, "Sound On", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Sound On", Toast.LENGTH_SHORT).show();
                     sound.setTag(new Boolean(false));
                 }
             }
         });
+    }
 
-        final ImageView imageView = (ImageView) findViewById(R.id.transparent);
-        final GifImageView gifImageView = (GifImageView) findViewById(R.id.slimeGif);
 
-        final RelativeLayout RL = (RelativeLayout) findViewById(R.id.relayout);
-        final RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT, // Width of TextView
-                RelativeLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+    // Create Damage Text and sets the font, size, text, position
+    public void createDamageText(int damageTaken, float x, float y){
+        int slideHeight = ThreadLocalRandom.current().nextInt(200, 400 + 1);
+        int slideWidth = ThreadLocalRandom.current().nextInt(-20, 20 + 1);
+        String damage = Integer.toString(damageTaken);
 
-        imageView.setOnTouchListener(new View.OnTouchListener() {
+        final TextView damageText = new TextView(getApplicationContext());
+        damageText.setLayoutParams(lp);
+        damageText.setSingleLine();
+        RL.addView(damageText);
+
+        // fadeIn/Out Animations
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator()); //add this
+        fadeIn.setDuration(10);
+
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator()); //and this
+        fadeOut.setStartOffset(175);
+        fadeOut.setDuration(350);
+
+        TranslateAnimation moveUp = new TranslateAnimation(0, slideWidth, 0, -slideHeight);
+        moveUp.setDuration(150);
+
+
+        AnimationSet animationSet = new AnimationSet(true);
+        animationSet.addAnimation(fadeIn);
+        animationSet.addAnimation(moveUp);
+
+        fadeOut.setAnimationListener(new TranslateAnimation.AnimationListener() {
+
             @Override
-            public boolean onTouch (View v, MotionEvent event){
-//                TextView damageText = (TextView) findViewById(R.id.textView1);
+            public void onAnimationStart(Animation animation) { }
 
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN: {
-                        if(damageFx.isPlaying()){
-                            damageFx.seekTo(0);
-                        }
-                        damageFx.start();
+            @Override
+            public void onAnimationRepeat(Animation animation) { }
 
-                        //Animations
-                        Animation fadeIn = new AlphaAnimation(0, 1);
-                        fadeIn.setInterpolator(new DecelerateInterpolator()); //add this
-                        fadeIn.setDuration(300);
-
-                        Animation fadeOut = new AlphaAnimation(1, 0);
-                        fadeOut.setInterpolator(new AccelerateInterpolator()); //and this
-                        fadeOut.setStartOffset(300);
-                        fadeOut.setDuration(300);
-                        final AnimationSet animationSet = new AnimationSet(true);
-                        animationSet.addAnimation(fadeIn);
-
-                        final TextView damageText = new TextView(getApplicationContext());
-                        damageText.setLayoutParams(lp);
-                        RL.addView(damageText);
-                        damageText.setTextAppearance(MainActivity.this, R.style.AudioFileInfoOverlayText);
-                        int x = (int) event.getX()-120;
-                        int y = (int) event.getY()-50;
-                        int slideHeight = ThreadLocalRandom.current().nextInt(80, 200 + 1);
-                        TranslateAnimation transAnimation= new TranslateAnimation(0, 0, 0, -slideHeight);
-                        transAnimation.setDuration(200);
-                        transAnimation.setStartOffset(50);
-                        animationSet.addAnimation(transAnimation);
-                        fadeOut.setAnimationListener(new TranslateAnimation.AnimationListener() {
-
-                            @Override
-                            public void onAnimationStart(Animation animation) { }
-
-                            @Override
-                            public void onAnimationRepeat(Animation animation) { }
-
-                            @Override
-                            public void onAnimationEnd(Animation animation)
-                            {
-                                damageText.setVisibility(View.GONE);
-                            }
-                        });
-                        animationSet.addAnimation(fadeOut);
-
-                        // Damage
-                        int randomNum = ThreadLocalRandom.current().nextInt(0, 999999 + 1); // nextInt is normally exclusive of the top value, so add 1 to make it inclusive
-                        String damage = Integer.toString(randomNum);
-                        damageText.setTextSize(25);
-
-                        if(randomNum>=900000){
-                            damageText.setTextSize(65);
-                        }else if (randomNum<900000 && randomNum>=75000){
-                            damageText.setTextSize(40);
-                        }else if (randomNum<500000){
-                            damage = "MISS";
-                        }
-                        int top=ContextCompat.getColor(MainActivity.this, R.color.TopText);
-                        int bottom=ContextCompat.getColor(MainActivity.this, R.color.BottomText);
-                        Shader textShader=new LinearGradient(0, 0, 0, damageText.getPaint().getTextSize(),
-                                new int[]{top ,bottom},
-                                new float[]{0, 1}, Shader.TileMode.CLAMP);//Assumes bottom and top are colors defined above
-                        damageText.getPaint().setShader(textShader);
-                        damageText.setShadowLayer(1, 0, 0, Color.BLACK);
-                        Typeface comic_sans = Typeface.createFromAsset(getAssets(),"comic-sans-ms-bold.ttf");
-                        damageText.setTypeface(comic_sans);
-
-                        //Start animation
-                        damageText.setX(x);
-                        damageText.setY(y);
-                        damageText.setText(damage);
-                        damageText.startAnimation(animationSet);
-//                        damageText.animate()
-//                                .translationY(damageText.getHeight())
-//                                .alpha(0.0f)
-//                                .setDuration(3000)
-//                                .setListener(new AnimatorListenerAdapter() {
-//                                    @Override
-//                                    public void onAnimationEnd(android.animation.Animator animation) {
-//                                        super.onAnimationEnd(animation);
-//                                        damageText.setVisibility(View.GONE);
-//                                    }
-//                                });
-                        gifImageView.setImageResource(R.drawable.kingslimehurt);
-                        break;
-                    }
-                    case MotionEvent.ACTION_UP: {
-                        gifImageView.setImageResource(R.drawable.kingslime_animation);
-                        break;
-                    }
-                }
-                return true;
+            @Override
+            public void onAnimationEnd(Animation animation)
+            {
+                damageText.setVisibility(View.GONE);
             }
         });
+        animationSet.addAnimation(fadeOut);
+
+        damageText.setTypeface(comic_sans);
+        damageText.setTextSize(normalDmgSize);
+        Shader textShader=new LinearGradient(0, 0, 0, damageText.getPaint().getTextSize(),
+                new int[]{dmgTop,dmgBottom},
+                new float[]{0, 1}, Shader.TileMode.CLAMP);
+        damageText.setShadowLayer(1.5f, 5.0f, 5.0f, Color.BLACK);
+
+        damageText.getPaint().setShader(textShader);
+        if(damageTaken>=900000){
+            damageText.setTextSize(critialDmgSize);
+            Shader critShader=new LinearGradient(0, 0, 0, damageText.getPaint().getTextSize(),
+                    new int[]{critTop, critBottom},
+                    new float[]{0, 1}, Shader.TileMode.CLAMP);
+            damageText.getPaint().setShader(critShader);
+        }else if (damageTaken<500000){
+            damage = " MISS ";
+        }
+        damageText.setText(damage);
+        // Position damageText to Click position
+        damageText.setX(x-200);
+        damageText.setY(y-150);
+        damageText.setVisibility(View.VISIBLE);
+        damageText.startAnimation(animationSet);
+    }
+
+
+    // Create AnimationSet for Damage text, returns animationSet
+    public AnimationSet createDamageTextAnimation(){
+        // fade Out Animations
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator()); //and this
+        fadeOut.setStartOffset(150);
+        fadeOut.setDuration(400);
+
+        int slideHeight = ThreadLocalRandom.current().nextInt(100, 200 + 1);
+        int slideWidth = ThreadLocalRandom.current().nextInt(-5, 5 + 1);
+        TranslateAnimation moveUp = new TranslateAnimation(0, slideWidth, 0, -slideHeight);
+        moveUp.setDuration(150);
+        moveUp.setStartOffset(50);
+
+        AnimationSet animationSet = new AnimationSet(true);
+        animationSet.addAnimation(moveUp);
+
+        return animationSet;
     }
 }
 
